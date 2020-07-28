071acb414bcba7180da342fecd79c1cb67a1d9cf
shantanu354 <kanhapatildurg@gmail.com>
2020-07-28 22:15:47 +0530

    Add ProfileActivity

---
+++ app/src/main/java/com/example/electedperson/ProfileActivity.java
@@ -0,0 +1,22 @@
+package com.example.electedperson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
