2645d3d0744f6bd2a3f04d9b41399214cccb4a05
Satyam <satyampatildurg@gmail.com>
2020-08-03 16:36:05 +0530

    initial commit

--- app/src/main/java/com/example/electedperson/HomeActivity.java
+++ app/src/main/java/com/example/electedperson/HomeActivity.java
@@ -5,47 +5,54 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.animation.EasingFunction;
 import com.github.mikephil.charting.charts.CombinedChart;
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.BubbleData;
-import com.github.mikephil.charting.data.BubbleDataSet;
-import com.github.mikephil.charting.data.BubbleEntry;
-import com.github.mikephil.charting.data.CandleData;
-import com.github.mikephil.charting.data.CandleDataSet;
-import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.data.ScatterData;
-import com.github.mikephil.charting.data.ScatterDataSet;
-import com.github.mikephil.charting.formatter.IFillFormatter;
-import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
@@ -57,7 +64,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SeekBar.OnSeekBarChangeListener,
+        OnChartValueSelectedListener  {
 
     private ActionBarDrawerToggle nToggle;
     NavigationView navigationView;
@@ -72,6 +80,11 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     protected final String[] months = new String[]{
             "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
     };
+    protected final String[] parties = new String[] {
+            "Severity 1", "Severity 2", "Severity 3", "Severity 4", "Severity 5"
+    };
+    private PieChart chart_Pie;
+    private SeekBar seekBarX, seekBarY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -179,6 +192,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                                                         chart.setData(data);
                                                         chart.invalidate();
                                                         updateChart();
+                                                        updateChart1();
                                                     }
                                                 }
                                             }
@@ -187,22 +201,176 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                         }
                     }
                 });
+
+
+        //working on the pie chart
+        seekBarX = findViewById(R.id.seekBar1);
+        seekBarY = findViewById(R.id.seekBar2);
+        seekBarX.setOnSeekBarChangeListener(this);
+        seekBarY.setOnSeekBarChangeListener(this);
+
+        chart_Pie = findViewById(R.id.piechart);
+        chart_Pie.setUsePercentValues(true);
+        chart_Pie.getDescription().setEnabled(false);
+        chart_Pie.setExtraOffsets(5, 10, 5, 5);
+
+        chart_Pie.setDragDecelerationFrictionCoef(0.95f);
+
+//        chart_Pie.setCenterText(generateCenterSpannableText());
+
+        chart_Pie.setDrawHoleEnabled(true);
+        chart_Pie.setHoleColor(Color.WHITE);
+
+        chart_Pie.setTransparentCircleColor(Color.WHITE);
+        chart_Pie.setTransparentCircleAlpha(110);
+
+        chart_Pie.setHoleRadius(58f);
+        chart_Pie.setTransparentCircleRadius(61f);
+
+        chart_Pie.setDrawCenterText(true);
+
+        chart_Pie.setRotationAngle(0);
+        // enable rotation of the chart_Pie by touch
+        chart_Pie.setRotationEnabled(true);
+        chart_Pie.setHighlightPerTapEnabled(true);
+
+        // chart_Pie.setUnit(" €");
+        // chart_Pie.setDrawUnitsInChart(true);
+
+        // add a selection listener
+        chart_Pie.setOnChartValueSelectedListener(this);
+
+        seekBarX.setProgress(5);
+        seekBarY.setProgress(60);
+
+        chart_Pie.animateY(1400, EaseInOutQuad);
+        // chart_Pie.spin(2000, 0, 360);
+
+        Legend pi = chart_Pie.getLegend();
+        pi.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        pi.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        pi.setOrientation(Legend.LegendOrientation.VERTICAL);
+        pi.setDrawInside(false);
+        pi.setXEntrySpace(7f);
+        pi.setYEntrySpace(0f);
+        pi.setYOffset(0f);
+
+        // entry label styling
+        chart_Pie.setEntryLabelColor(Color.WHITE);
+        chart_Pie.setEntryLabelTextSize(12f);
     }
 
+    //pie chart
+    private void setData(int count, float range) {
+        ArrayList<PieEntry> entries_Pie = new ArrayList<>();
+
+        // NOTE: The order of the entries_Pie when being added to the entries_Pie array determines their position around the center of
+        // the chart_Pie.
+        for (int i = 0; i < count ; i++) {
+            entries_Pie.add(new PieEntry((float) ((Math.random() * range) + range / 5),
+                    parties[i % parties.length],
+                    getResources().getDrawable(R.drawable.star)));
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries_Pie, "Severity Level");
+
+        dataSet.setDrawIcons(false);
+
+        dataSet.setSliceSpace(3f);
+        dataSet.setIconsOffset(new MPPointF(0, 40));
+        dataSet.setSelectionShift(5f);
+
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        dataSet.setColors(colors);
+        //dataSet.setSelectionShift(0f);
+
+        PieData data_Pie = new PieData(dataSet);
+        data_Pie.setValueFormatter(new PercentFormatter());
+        data_Pie.setValueTextSize(11f);
+        data_Pie.setValueTextColor(Color.WHITE);
+        chart_Pie.setData(data_Pie);
+
+        // undo all highlights
+        chart_Pie.highlightValues(null);
+
+        chart_Pie.invalidate();
+    }
+
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        setData(seekBarX.getProgress(), seekBarY.getProgress());
+    }
+
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+        if (e == null)
+            return;
+        Log.i("VAL SELECTED",
+                "Value: " + e.getY() + ", index: " + h.getX()
+                        + ", DataSet index: " + h.getDataSetIndex());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("PieChart", "nothing selected");
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+    public static final EasingFunction EaseInOutQuad = new EasingFunction() {
+        public float getInterpolation(float input) {
+            input *= 2f;
+
+            if (input < 1f) {
+                return 0.5f * input * input;
+            }
+
+            return -0.5f * ((--input) * (input - 2f) - 1f);
+        }
+    };
 
     private LineData generateLineData() {
 
         LineData d = new LineData();
         LineDataSet set = new LineDataSet(entries, "Fixed Pothole");
-        set.setColor(Color.rgb(240, 238, 70));
+        set.setColor(Color.rgb(139, 216, 189));
         set.setLineWidth(2.5f);
-        set.setCircleColor(Color.rgb(240, 238, 70));
+        set.setCircleColor(Color.rgb(139, 216, 189));
         set.setCircleRadius(5f);
-        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setFillColor(Color.rgb(139, 216, 189));
         set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
         set.setDrawValues(true);
         set.setValueTextSize(10f);
-        set.setValueTextColor(Color.rgb(240, 238, 70));
+        set.setValueTextColor(Color.rgb(139, 216, 189));
 
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
         d.addDataSet(set);
@@ -214,20 +382,20 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     private BarData generateBarData() {
 
         BarDataSet set1 = new BarDataSet(entries1, "");
-        set1.setColor(Color.rgb(60, 220, 78));
+        set1.setColor(Color.rgb(36, 54, 101));
         set1.setValueTextColor(Color.rgb(255, 255, 255));
         set1.setValueTextSize(10f);
         set1.setAxisDependency(YAxis.AxisDependency.LEFT);
 
         BarDataSet set2 = new BarDataSet(entries2, "Recorded Pothole");
-        set2.setColor(Color.rgb(60, 220, 78));
-        set2.setValueTextColor(Color.rgb(60, 220, 78));
+        set2.setColor(Color.rgb(36, 54, 101));
+        set2.setValueTextColor(Color.rgb(36, 54, 101));
         set2.setValueTextSize(10f);
         set2.setAxisDependency(YAxis.AxisDependency.LEFT);
 
         float groupSpace = 0.06f;
-        float barSpace = 0.02f; // x2 dataset
-        float barWidth = 0.45f; // x2 dataset
+        float barSpace = 0.02f;
+        float barWidth = 0.45f;
 
         BarData d = new BarData(set1, set2);
         d.setBarWidth(barWidth);
@@ -241,14 +409,27 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     private void updateChart(){
         // Update the text in a center of the chart:
         TextView numberOfCals = findViewById(R.id.number_of_calories);
-        numberOfCals.setText(String.valueOf(calsBurned) + " / " + calsConsumed);
+        numberOfCals.setText(String.valueOf(calsConsumed) + " / " + calsBurned);
 
         // Calculate the slice size and update the pie chart:
-        ProgressBar pieChart = findViewById(R.id.stats_progressbar);
+        ProgressBar pieChart_1 = findViewById(R.id.stats_progressbar);
         double d = (double) calsBurned / (double) calsConsumed;
         int progress = (int) (d * 100);
-        pieChart.setProgress(progress);
+        pieChart_1.setProgress(progress);
     }
+
+    private void updateChart1(){
+        // Update the text in a center of the chart:
+        TextView numberOfCals = findViewById(R.id.number_of_calories1);
+        numberOfCals.setText(String.valueOf(168) + " / " + 347);
+
+        // Calculate the slice size and update the pie chart:
+        ProgressBar pieChart_1 = findViewById(R.id.stats_progressbar1);
+        double d = (double) 168 / (double) 347;
+        int progress = (int) (d * 100);
+        pieChart_1.setProgress(progress);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
